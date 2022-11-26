03ace5e2d606a938eb3b42e2a6c0335d476653ca
simo1231211 <miroafrin@gmail.com>
2022-11-26 23:00:13 +0300

    first commit

---
+++ bin/num.dart
@@ -0,0 +1,14 @@
+import 'package:num/num.dart' as num;
+
+void main(List<String> arguments) {
+  int num1,num2,result;
+  for(num1=1;num1<=10;num1++){
+    for(num2=1;num2<=10;num2++){
+    
+   
+      result=num1*num2;    
+      print("$num1 * $num2 = $result");
+  }
+}
+  
+}
---
+++ test/num_test.dart
@@ -0,0 +1,8 @@
+import 'package:num/num.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
